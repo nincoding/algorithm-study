7b6e38a77dc5966672c42582895f3a59c2ffe08a
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-01-21 23:26:10 +0900

    feat(PGS): 레벨1_완주하지 못한 선수
    https://school.programmers.co.kr/learn/courses/30/lessons/42576

---
+++ rsuubinn/PGS_완주하지못한선수.js
@@ -0,0 +1,9 @@
+function solution(participant, completion) {
+    participant = participant.sort();
+    completion = completion.sort();
+    
+    for(let i = 0; i < participant.length; i++) {
+        if(participant[i] !== completion[i]) return participant[i];
+    }
+    
+}
