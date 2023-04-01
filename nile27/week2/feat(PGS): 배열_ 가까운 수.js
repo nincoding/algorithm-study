2d5ee957f1f00477b18eb1b3d5d6d7200b2ce695
nile27 <ddj03104@gmail.com>
2023-04-01 10:36:33 +0900

    feat(PGS) : 배열 가까운 수

--- nile27/week2/feat(PGS): 배열_ 가까운 수.js
+++ nile27/week2/feat(PGS): 배열_ 가까운 수.js
@@ -1,7 +1,7 @@
 /** 가까운 수 (누적 합)
  * LV.0
  * 정수 배열 array와 정수 n이 매개변수로 주어질 때, array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
- *  입출력 예시
+ *  input/output
  * array = [3,10,28] n = 20 result = 28
  */
 function solution(array, n) {
